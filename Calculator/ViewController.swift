c2836317060597aad6c445f4efc4048c4956dfd7
dldydrhkd <48857296+dldydrhkd@users.noreply.github.com>
2020-03-13 02:51:49 +0900

    making calculator ui
    digit buttons are made

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        print("touched \(digit) digit")
     }
-
-
+    
 }
-
